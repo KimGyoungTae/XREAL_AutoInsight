ba414bc7e50c181782d46a6d3057f984d68ae340
wave-sound <lunar105630@gmail.com>
2023-12-25 18:25:20 +0900

    [Refactor] 디버그에 맞게 PopUp 수정

--- Assets/Scripts/SeongJae/MarkerModel.cs
+++ Assets/Scripts/SeongJae/MarkerModel.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -6,7 +7,8 @@ public class MarkerModel : MonoBehaviour
 {
     // 추후 데이터 테이블을 사용할 때 각 차량별 마커의 수를 가져옴.
     [Header("Debug")]
-    [SerializeField] private MarkerCanvas[] markerCanvas;
+    [SerializeField] private MarkerCanvas[] markerCanvases;
+    [SerializeField] private Vector3[] popUpPosition; 
 
     // 프리팹의 종류가 다를 경우 딕셔너리에 등록 후, Instatiate로 사용.
     /*
@@ -20,10 +22,14 @@ public class MarkerModel : MonoBehaviour
     }
     public void InitSettings()
     {   
-        // 현재는 데이터 테이블이 없어 하드코딩으로 진행함.
-        PopUpPanel popUpPanel = markerCanvas[0].GetComponentInChildren<PopUpPanel>();
-        popUpPanel.gameObject.SetActive(false);
-        markerCanvas[0].InitSettings(new Vector3(0, 1, 3.5f), popUpPanel);
+        // 현재는 데이터 테이블이 없어 캔버스를 로드하는 부분의 구현을 하드코딩으로 진행함.
+        for(int i = 0; i < popUpPosition.Length; i++)
+        {
+            MarkerCanvas markerCanvas = Instantiate(markerCanvases[0].gameObject).GetComponent<MarkerCanvas>();
+            PopUpPanel popUpPanel = markerCanvas.GetComponentInChildren<PopUpPanel>(true);
+            popUpPanel.gameObject.SetActive(false);
+            markerCanvas.InitSettings(popUpPosition[i], popUpPanel);
+        }
     }
 
     /*private void AddDictionary(Dictionary<string, GameObject> prefabDictionary, string prefabName)
